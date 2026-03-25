2d431de99710c549f6597d27f6a06e2a73148e99
Vishwajeet Deshmukh <vishwajeetdeshmukh.dev@gmail.com>
2026-03-25 21:01:35 +0530

    Display computer's choice in Rock-Paper-Scissors game
    Added computer choice output to the game.

--- Rock_Paper_Scissor_game.py
+++ Rock_Paper_Scissor_game.py
@@ -7,6 +7,7 @@ select = random.choice(computer_selection)
 #Input form User
 print("Rock","Paper","Scissors")
 User_Selection = input("Please enter your choice: ")
+print(f"Computer Choice: {select}")
 
 #Condition Checking by Rules (Who Win or Lose)
 if User_Selection == select:
@@ -19,3 +20,4 @@ elif User_Selection == "Paper" and select == "Rock":
     print("You win")
 else:
     print("You lose")
+
