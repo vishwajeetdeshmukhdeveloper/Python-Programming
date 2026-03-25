ec55da20d49c1bb5fedf9c329f0a2f7b660d486f
Vishwajeet Deshmukh <vishwajeetdeshmukh.dev@gmail.com>
2026-03-25 20:57:35 +0530

    Implement Rock-Paper-Scissors game logic

---
+++ Rock_Paper_Scissor_game.py
@@ -0,0 +1,21 @@
+import random
+
+# Selection of Computer
+computer_selection = ["Rock","Paper","Scissors"]
+select = random.choice(computer_selection)
+
+#Input form User
+print("Rock","Paper","Scissors")
+User_Selection = input("Please enter your choice: ")
+
+#Condition Checking by Rules (Who Win or Lose)
+if User_Selection == select:
+    print("Tie")
+elif User_Selection == "Rock" and select == "Scissors":
+    print("You win")
+elif User_Selection == "Scissors" and select == "Paper":
+    print("You win")
+elif User_Selection == "Paper" and select == "Rock":
+    print("You win")
+else:
+    print("You lose")
