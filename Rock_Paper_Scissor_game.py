53a18f5d7d13c3d5bf0990da894e01ab22b93d07
Vishwajeet Deshmukh <vishwajeetdeshmukh.dev@gmail.com>
2026-03-25 21:15:42 +0530

    Update Rock_Paper_Scissor_game.py

--- Rock_Paper_Scissor_game.py
+++ Rock_Paper_Scissor_game.py
@@ -7,9 +7,76 @@ select = random.choice(computer_selection)
 #Input form User
 print("Rock","Paper","Scissors")
 User_Selection = input("Please enter your choice: ")
-print(f"Computer Choice: {select}")
 
+if User_Selection == "Rock":
+    print('''   
+            _______
+        ---'    ____)
+               (_____)
+               (_____)
+                (____)
+         ---.__(___)
+                 ''')
+
+elif User_Selection == "Paper":
+    print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+
+elif User_Selection == "Scissors":
+    print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+
+
+print(f"Computer Choice: {select}")
 #Condition Checking by Rules (Who Win or Lose)
+if select == "Rock":
+    print('''   
+        _______
+    ---'    ____)
+           (_____)
+           (_____)
+            (____)
+     ---.__(___)
+             ''')
+
+
+elif select == "Paper":
+    print("""
+         _______
+    ---'    ____)____
+               ______)
+              _______)
+             _______)
+    ---.__________)
+    """)
+
+elif select == "Scissors":
+    print("""
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    """)
+
+
+
+
+
+
 if User_Selection == select:
     print("Tie")
 elif User_Selection == "Rock" and select == "Scissors":
@@ -20,4 +87,5 @@ elif User_Selection == "Paper" and select == "Rock":
     print("You win")
 else:
     print("You lose")
+
 
